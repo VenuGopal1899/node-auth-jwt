a9c0d144ecbe87a48189528cc27586d0d68afa6f
VenuGopal1899 <venumalladi18@gmail.com>
2021-05-05 08:36:17 +0530

    Login page added

--- public/login.js
+++ public/login.js
@@ -1,33 +1,31 @@
-function mainFunction() {
-	var user = document.getElementById("username");
-	var pass = document.getElementById("password");
+const form = document.getElementById("login-form");
 
-	/*======Username field autofill events======*/
-	user.onfocus = function () {
-		if (user.value === "Username") {
-			user.value = "";
-		}
-	};
-	user.onblur = function () {
-		if (user.value === "") {
-			user.value = "";
-		}
-	};
-	/*==========================================*/
-	/*======Password field autofill events======*/
-	pass.onfocus = function () {
-		if (pass.value === "Password") {
-			pass.value = "";
-		}
-	};
-	pass.onblur = function () {
-		if (pass.value === "") {
-			pass.value = "Password";
-		}
-	};
-	/*==========================================*/
+form.addEventListener('submit', loginUser);
+
+async function loginUser(e){
+	e.preventDefault();
+	const ign = document.getElementById("ign").value;
+	const password = document.getElementById("password").value;
+
+	document.getElementById("err-message").innerHTML = "";
+
+	const result = await fetch('/api/login', {
+		method: 'POST',
+		headers: {
+			'Content-type' : 'application/json'
+		},
+		body: JSON.stringify({
+			ign, password
+		})
+	}).then(res => res.json());
+
+	if(result.status === 'ok'){
+		// console.log('Got the JWT Access Token and Refresh Token', result);
+		// accessToken and refreshToken and status : 'ok', will be sent as result
+		localStorage.setItem("JWT", result.accessToken);
+		window.location.href = "http://localhost:4000/homepage";
+	} else {
+        document.getElementById("err-message").innerHTML = result.error;
+    }
 }
 
-window.onload = function () {
-	mainFunction();
-}
